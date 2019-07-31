9ef88575f7aedaa2fd29ba916b0f8fe3f6c82ca9
yasmine9 <YasmineYas123>
2019-07-31 19:36:03 +0100

    Merge branch 'master' of https://github.com/Moumene/yassir_mobile
    # Conflicts:
    #	android/app/src/main/java/com/example/stage_part1/BottomSheetDialog.java

--- android/app/src/main/java/com/example/stage_part1/MainActivity.java
+++ android/app/src/main/java/com/example/stage_part1/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.stage_part1;
 
 import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.os.PowerManager;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -14,7 +18,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            Intent intent = new Intent();
+            String packageName = getPackageName();
+            PowerManager pm = (PowerManager) getSystemService(POWER_SERVICE);
+            if (!pm.isIgnoringBatteryOptimizations(packageName)) {
+                intent.setAction(Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS);
+                intent.setData(Uri.parse("package:" + packageName));
+                startActivity(intent);
+            }
+        }
 
         Button nBtn = (Button) findViewById(R.id.btn);
         nBtn.setOnClickListener(new View.OnClickListener() {
